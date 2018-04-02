77743fdc657883c049ecd1b2961644cb2218478b
Jonas Barte <jonas@barte.se>
2018-04-02 16:03:56 +0200

    Chapter 7

---
+++ scripts/main.js
@@ -0,0 +1,61 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails(imageUrl, textTitle) {
+  'use strict';
+
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailImage.setAttribute('src', imageUrl);
+  detailTitle.textContent = textTitle;
+
+}
+
+
+function changeRandomUrl() {
+  var images = document.querySelectorAll('[data-image-url]');
+  var numberToSwitch = Math.floor(Math.random()*images.length);
+  var image = images[numberToSwitch];
+  var tacoCat = 'https://i.imgur.com/uajbPii.jpg';
+  image.setAttribute('data-image-url', tacoCat);
+}
+
+function imageFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-url');
+}
+
+function titleFromThumb(thumbnail) {
+  'use strict';
+  return thumbnail.getAttribute('data-image-title');
+}
+
+function setDetailsFromThumb(thumbnail) {
+  'use strict';
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail))
+}
+
+function addThumbClickHandler(thumb) {
+  'use strict';
+  thumb.addEventListener('click', function(event){
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+  });
+}
+
+function getThumbnailsArray() {
+  'use strict';
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailsArray = [].slice.call(thumbnails);
+  return thumbnailsArray;
+}
+
+function initializeEvents() {
+  'use strict';
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+  changeRandomUrl();
+}
+
+initializeEvents();
